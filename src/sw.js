dfae6b413867d495fb09ac3552ca9b7458e93917
Paul Petrov <paul@bargamut.ru>
2019-04-02 02:11:37 +0300

    Добавить файл ServiseWorker'а

---
+++ src/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = `STATIC_V1.0`;
+
+const putToWSCache = (evt, response) => {
+  caches.open(CACHE_NAME)
+    .then((cache) => cache.put(evt.request, response));
+};
+
+self.addEventListener(`install`, (evt) => {
+  const openCache = caches.open(CACHE_NAME)
+    .then((cache) => {
+      cache.addAll([
+        `./`,
+        `./index.html`,
+        `./bundle.js`,
+        `./css/normalize.css`,
+        `./css/main.css`
+      ]);
+    });
+
+  evt.waitUntil(openCache);
+});
+
+self.addEventListener(`fetch`, (evt) => {
+  evt.respondWith(
+      fetch(evt.pequest)
+        .then((response) => {
+          putToWSCache(evt, response);
+
+          return response.clone();
+        })
+        .catch(() => {
+          return caches.match(evt.request)
+            .then((response) => {
+              return response.clone();
+            })
+            .catch((err) => {
+              throw err;
+            });
+        })
+  );
+});
